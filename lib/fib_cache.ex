ed46f4e01f28669170fa918d5575842d297ac7d1
Antonio Britto <antonioabritto@gmail.com>
2018-11-04 20:16:06 -0200

    mix new fib_cache

---
+++ lib/fib_cache.ex
@@ -0,0 +1,18 @@
+defmodule FibCache do
+  @moduledoc """
+  Documentation for FibCache.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FibCache.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
